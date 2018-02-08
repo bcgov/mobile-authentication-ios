9e8fe0945fc5deb3b9447c0905619861b0537fe3
Jason C. Leach <jason.leach@fullboar.ca>
2018-02-08 15:37:05 -0800

    Add basic framework project

---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+project 'SingleSignOn.xcodeproj'
+platform :ios, '9.0'
+use_frameworks!
+
+target 'SingleSignOn' do
+    pod 'Alamofire'
+    pod 'SwiftKeychainWrapper'
+end
---
+++ SingleSignOn/SingleSignOn.h
@@ -0,0 +1,19 @@
+//
+//  SingleSignOn.h
+//  SingleSignOn
+//
+//  Created by Jason Leach on 2018-02-08.
+//  Copyright © 2018 Jason Leach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SingleSignOn.
+FOUNDATION_EXPORT double SingleSignOnVersionNumber;
+
+//! Project version string for SingleSignOn.
+FOUNDATION_EXPORT const unsigned char SingleSignOnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SingleSignOn/PublicHeader.h>
+
+
